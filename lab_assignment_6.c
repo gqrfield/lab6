ed72ac6599ddf5874e805f4ba58afb42005361ff
Owen <oweng325@gmail.com>
2024-02-22 18:34:55 -0500

    Fixed edge case

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -3,7 +3,7 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
-	if (low >= high) return -1;
+	if (low > high) return -1;
 	
 	int mid = (low+high) / 2;
 	if (numbers[mid] == value) {
